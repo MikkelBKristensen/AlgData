6860ff6c479a544bcae489711d04b14529fa2b15
Mikkel <110497736+MikkelBKristensen@users.noreply.github.com>
2023-02-10 16:10:57 +0100

    Updates + New handin

--- AlgDataHandIns/src/Balance.java
+++ AlgDataHandIns/src/Balance.java
@@ -1,22 +1,50 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import edu.princeton.cs.algs4.Stack;
 
 public class Balance {
+
+    static boolean isBalanced(String[] input) throws NoSuchElementException {
+        Stack<String> par = new Stack<>();
+        if ((input.length % 2) != 0) {
+            return false;
+        }
+        try {
+            for (int i = 0; i < input.length; i++) {
+                switch (input[i]) {
+                    case "(" : par.push("(");
+                        break;
+                    case "[" : par.push("[");
+                        break;
+                    case ")" : String value1 = par.pop();
+                        if (value1 == null || value1 == "[") {
+                        return false;
+                    };
+                        break;
+                    case "]" : String value2 = par.pop();
+                        if (value2 == null || value2 == "(") {
+                            return false;
+                    };
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            if (!par.isEmpty()) {
+                return false;
+            }
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        if (input.equals("")) {
-            System.out.println("1");
-        } else if ((input.length() % 2) != 0) {
-            System.out.println("0");
+        String[] input = sc.nextLine().split("");
+        if (isBalanced(input)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
         }
-        char[] charArr = input.toCharArray();
-        for (char c : charArr) {
-            System.out.println(c);
-        }
-
-
-
-
     }
 }
